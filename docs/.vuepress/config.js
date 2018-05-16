cac3d488997e29200f6feda755bc2b21602f6d19
Imamachi-n <imamachigatugatu@hotmail.co.jp>
2018-05-17 07:35:18 +0900

    test.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  base: '/ngs-ninja/',
+  title: 'NGS Ninja',
+  description: 'Next-generation sequencing data analysis wiki',
+  dest: 'public',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Get Started', link: '/About/' },
+      { text: 'Tools', link: '/Tools/' },
+      { text: 'Workflow', link: '/Workflow/' },
+    ],
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: 'Imamachi-n/ngs-ninja',
+    // Customising the header label
+    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+    repoLabel: 'GitHub',
+
+    // Optional options for generating "Edit this page" link
+
+    // if your docs are in a different repo from your main project:
+    docsRepo: 'Imamachi-n/ngs-ninja',
+    // if your docs are in a specific branch (defaults to 'master'):
+    docsBranch: 'master',
+    // defaults to true, set to false to disable
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: 'Edit this page on GitHub',
+
+    theme: 'vue',
+    lastUpdated: 'Last Updated',
+  },
+  head: [
+    ['link', { rel: 'icon', href: `/hero.png` }]
+  ]
+}
---
+++ docs/About/README.md
@@ -0,0 +1,29 @@
+---
+prev: false
+next: false
+---
+
+# テストです
+## テストです
+### テストです
+[Home](/) 
+
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+| col 2 is      | centered      |   $12 |
+| zebra stripes | are neat      |    $1 |
+
+::: tip 注目
+This is a tip
+:::
+
+::: warning 警告
+This is a warning
+:::
+
+::: danger 注意
+This is a dangerous warning
+:::
+
+![An image](./image.png)
