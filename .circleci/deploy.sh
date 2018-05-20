83f958abafefc845e98b32d5dfcd707ad106ec3a
Imamachi-n <imamachigatugatu@hotmail.co.jp>
2018-05-20 21:29:06 +0900

    test

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -9,27 +9,24 @@ set -o errexit
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
-# SOURCE_BRANCH="master"
-# TARGET_BRANCH="gh-pages"
-# GITHUB_EMAIL="circleci@gh-pages.com"
-# GITHUB_NAME="CircleCI"
-
-# set gitconfig
+# Set gitconfig
 git config --global user.email ${GITHUB_EMAIL}
 git config --global user.name ${GITHUB_NAME}
 
-# 
+# Clone master branch
 git clone ${CIRCLE_REPOSITORY_URL} out
 cd out
 git checkout ${TARGET_BRANCH} || git checkout --orphan ${TARGET_BRANCH}
 git rm -rf .
 cd ..
 
+# Build vuepress docs
 yarn global add vuepress
 yarn docs:build
 cp -a public/. out/.
 mkdir -p out/.circleci && cp -a .circleci/. out/.circleci/.
 
+# Deploy docs to gh-pages branch
 cd out
 git add -A
 git commit -m "Automated deployment to GitHub Pages" --allow-empty
